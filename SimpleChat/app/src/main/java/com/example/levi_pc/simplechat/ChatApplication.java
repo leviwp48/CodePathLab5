b0070ad14cbbacc69335b36a2f79baf314ee3f46
leviwp48 <leviwp48@gmail.com>
2018-12-16 02:15:58 -0800

    Adding finished Lab

---
+++ SimpleChat/app/src/main/java/com/example/levi_pc/simplechat/ChatApplication.java
@@ -0,0 +1,39 @@
+package com.example.levi_pc.simplechat;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseLiveQueryClient;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.SubscriptionHandling;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+public class ChatApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ParseObject.registerSubclass(Message.class);
+
+        // Use for monitoring Parse network traffic
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        // Can be Level.BASIC, Level.HEADERS, or Level.BODY
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        builder.networkInterceptors().add(httpLoggingInterceptor);
+
+        // set applicationId and server based on the values in the Heroku settings.
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("codepath-chat-client") // should correspond to APP_ID env variable
+                .clientBuilder(builder)
+                .server("http://codepath-chat-client.herokuapp.com/parse").build());
+
+
+
+    }
+
+}
